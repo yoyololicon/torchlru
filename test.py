7c9863702f33aff9f4bcf0c340dfc837391ba720
Chin-Yun Yu <lolimaster.cs03@nctu.edu.tw>
2024-04-23 23:42:39 +0800

    add gradient test

--- test.py
+++ test.py
@@ -1,14 +1,38 @@
 import pytest
 import numpy as np
 import torch
+from torch.autograd import gradcheck, gradgradcheck
 from numba import cuda
 
 from torchlpc import sample_wise_lpc
-from parallel_scan import compute_linear_recurrence
+
+from .parallel_scan import compute_linear_recurrence
+from .recurrence import Recurrence
+
+
+@pytest.mark.parametrize("decay_requires_grad", [True])
+@pytest.mark.parametrize("impulse_requires_grad", [True, False])
+@pytest.mark.parametrize("initial_state_requires_grad", [True, False])
+@pytest.mark.parametrize("dtype", [torch.float64, torch.complex128])
+def test_grad(
+    decay_requires_grad, impulse_requires_grad, initial_state_requires_grad, dtype
+):
+    decay = torch.rand(
+        4, 50, requires_grad=decay_requires_grad, dtype=dtype, device="cuda"
+    )
+    impulse = torch.randn(
+        4, 50, requires_grad=impulse_requires_grad, dtype=dtype, device="cuda"
+    )
+    initial_state = torch.randn(
+        4, requires_grad=initial_state_requires_grad, dtype=dtype, device="cuda"
+    )
+
+    assert gradcheck(Recurrence.apply, (decay, impulse, initial_state))
+    assert gradgradcheck(Recurrence.apply, (decay, impulse, initial_state))
 
 
 @pytest.mark.parametrize("n_dims", [16, 64])
-@pytest.mark.parametrize("n_steps", [4096, 2**14, 2**16])
+@pytest.mark.parametrize("n_steps", [4096, 2**14 - 1, 2**16 - 1])
 @pytest.mark.parametrize("dtype", [np.float32, np.float64, np.complex64, np.complex128])
 def test_equivalent(n_dims, n_steps, dtype):
     decays = np.random.rand(n_dims, n_steps).astype(dtype) * 2 - 1
